945dc57d2cec02672fc9bf49d439a34f3e90ad18
Khang Hoang <hoangtrieukhang@gmail.com>
2016-07-02 17:20:49 +0700

    finish with custom player

--- xhub/index.ios.js
+++ xhub/index.ios.js
@@ -13,7 +13,8 @@ import React, {
   StyleSheet,
   ActivityIndicatorIOS,
   InteractionManager,
-  WebView
+  WebView,
+  TouchableOpacity
 } from 'react-native';
 import WebViewBridge from 'react-native-webview-bridge';
 import _ from 'lodash';
@@ -23,70 +24,178 @@ class xhub extends Component {
 
   constructor() {
     super();
+    this.state = {
+      rate: 1,
+      volume: 1,
+      muted: false,
+      resizeMode: 'contain',
+      duration: 0.0,
+      currentTime: 0.0,
+      controls: false,
+      paused: true,
+      skin: 'custom',
+      movieDirectURL: null
+    };
   }
 
   componentDidMount() {
     fetch('http://awesome-xhub.herokuapp.com/getMovie?url=http://xonline.tv/watch-tek-080-thick-kiss-sex-mikami-yua-that-caramel-saliva-2026.html')
-    .then(response => response._bodyText)
-    .then(JSON.parse)
+    .then(response => {
+      return response.text();
+    })
+    .then(data => JSON.parse(data))
     .then(data => {
-      console.log(data);
       this.setState({
         movieDirectURL: data.movie[0].file
       })
     })
-    .catch(console.log)
+    .catch(err => console.log(err))
   }
 
-  onValueChange(value) {
+  onProgress = (data) => {
+    this.setState({currentTime: data.currentTime});
+  }
+
+  onLoad = (videoDetails) => {
+    this.setState({
+      duration: videoDetails.duration
+    });
+  }
+
+  renderSkinControl(skin) {
+    const isSelected = this.state.skin == skin;
+    const selectControls = skin == 'native' || skin == 'embed';
+    return (
+      <TouchableOpacity onPress={() => { this.setState({
+          controls: selectControls,
+          skin: skin
+        }) }}>
+        <Text style={[styles.controlOption, {fontWeight: isSelected ? "bold" : "normal"}]}>
+          {skin}
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+
+  renderRateControl(rate) {
+    const isSelected = (this.state.rate == rate);
+
+    return (
+      <TouchableOpacity onPress={() => { this.setState({rate: rate}) }}>
+        <Text style={[styles.controlOption, {fontWeight: isSelected ? "bold" : "normal"}]}>
+          {rate}
+        </Text>
+      </TouchableOpacity>
+    )
+  }
+
+  renderResizeModeControl(resizeMode) {
+    const isSelected = (this.state.resizeMode == resizeMode);
+
+    return (
+      <TouchableOpacity onPress={() => { this.setState({resizeMode: resizeMode}) }}>
+        <Text style={[styles.controlOption, {fontWeight: isSelected ? "bold" : "normal"}]}>
+          {resizeMode}
+        </Text>
+      </TouchableOpacity>
+    )
+  }
+
+  getCurrentTimePercentage() {
+    if (this.state.currentTime > 0) {
+      return parseFloat(this.state.currentTime) / parseFloat(this.state.duration);
+    } else {
+      return 0;
+    }
+  }
+
+  renderVolumeControl(volume) {
+    const isSelected = (this.state.volume == volume);
+
+    return (
+      <TouchableOpacity onPress={() => { this.setState({volume: volume}) }}>
+        <Text style={[styles.controlOption, {fontWeight: isSelected ? "bold" : "normal"}]}>
+          {volume * 100}%
+        </Text>
+      </TouchableOpacity>
+    )
+  }
+
+  renderCustomSkin() {
+    const flexCompleted = this.getCurrentTimePercentage() * 100;
+    const flexRemaining = (1 - this.getCurrentTimePercentage()) * 100;
+
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.fullScreen} onPress={() => {this.setState({paused: !this.state.paused})}}>
+          <Video
+            ref='videoPlayer'
+            source={{uri:this.state.movieDirectURL}}
+            style={styles.fullScreen}
+            rate={this.state.rate}
+            paused={this.state.paused}
+            volume={this.state.volume}
+            muted={this.state.muted}
+            resizeMode={this.state.resizeMode}
+            onLoad={this.onLoad}
+            onProgress={this.onProgress}
+            onEnd={() => { AlertIOS.alert('Done!') }}
+            repeat={true}
+          />
+        </TouchableOpacity>
+
+        <View style={styles.controls}>
+          <View style={styles.generalControls}>
+            <View style={styles.skinControl}>
+              {this.renderSkinControl('custom')}
+              {this.renderSkinControl('native')}
+              {this.renderSkinControl('embed')}
+            </View>
+          </View>
+          <View style={styles.generalControls}>
+            <View style={styles.rateControl}>
+              {this.renderRateControl(0.5)}
+              {this.renderRateControl(1.0)}
+              {this.renderRateControl(2.0)}
+            </View>
+
+            <View style={styles.volumeControl}>
+              {this.renderVolumeControl(0.5)}
+              {this.renderVolumeControl(1)}
+              {this.renderVolumeControl(1.5)}
+            </View>
+
+            <View style={styles.resizeModeControl}>
+              {this.renderResizeModeControl('cover')}
+              {this.renderResizeModeControl('contain')}
+              {this.renderResizeModeControl('stretch')}
+            </View>
+          </View>
+
+          <View style={styles.trackingControls}>
+            <View style={styles.progress}>
+              <Slider
+                style={styles.slider}
+                onValueChange={this.onValueChange}
+              />
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
+  onValueChange = (value) => {
     let duration = _.get(this, "state.duration");
     if(duration) {
       this.refs.videoPlayer.seek(value * this.state.duration);
     }
   }
 
-  onLoad(videoDetails) {
-    this.setState({
-      duration: videoDetails.duration
-    });
-  }
-
-  setTime(videoDetails) {
-    this.setState({
-      currentTime: videoDetails.currentTime
-    });
-  }
-
-  videoError(info) {
-    console.log(info);
-  }
-
   render() {
 
     if(_.get(this, 'state.movieDirectURL')) {
-      return (
-        <View style={{flex: 1, backgroundColor: "white"}}>
-          <Video source={{uri: this.state.movieDirectURL}}
-            rate={1.0}
-            volume={1.0}
-            muted={false}
-            paused={false}
-            onLoadStart={this.loadStart}
-            onLoad={this.onLoad.bind(this)}
-            onProgress={this.setTime.bind(this)}
-            onEnd={this.onEnd}
-            onError={this.videoError.bind(this)}
-            style={styles.backgroundVideo}
-            ref="videoPlayer"
-          />
-          <View style={{position: "relative", height: 20, flex: 1, bottom: 20, backgroundColor: 'rgba(0,0,0,0)'}}>
-            <Slider 
-              style={styles.slider} 
-              onValueChange={this.onValueChange.bind(this)} 
-            />
-          </View>
-        </View>
-      )
+      return this.renderCustomSkin();
     }
 
     return (
@@ -102,21 +211,74 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'black',
   },
-  webview: {
+  fullScreen: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+  controls: {
+    backgroundColor: "transparent",
+    borderRadius: 5,
+    position: 'absolute',
+    bottom: 44,
+    left: 4,
+    right: 4,
+  },
+  progress: {
     flex: 1,
-    opacity: 0
+    flexDirection: 'row',
+    borderRadius: 3,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  innerProgressCompleted: {
+    height: 20,
+    backgroundColor: '#cccccc',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  innerProgressRemaining: {
+    height: 20,
+    backgroundColor: '#2C2C2C',
+  },
+  generalControls: {
+    flex: 1,
+    flexDirection: 'row',
+    overflow: 'hidden',
+    paddingBottom: 10,
+  },
+  skinControl: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  rateControl: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  volumeControl: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  resizeModeControl: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  controlOption: {
+    alignSelf: 'center',
+    fontSize: 11,
+    color: "white",
+    paddingLeft: 2,
+    paddingRight: 2,
+    lineHeight: 12,
+  },
+  nativeVideoControls: {
+    top: 184,
+    height: 300
   },
   slider: {
     height: 10,
@@ -125,15 +287,6 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     marginRight: 20,
     marginBottom: 20,
-  },
-  backgroundVideo: {
-    position: 'absolute',
-    backgroundColor: '#000000',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    flex: 1
   }
 });
 
